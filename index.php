161732e754d942adb11d8d83a522b34a7c877c61
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2022-12-10 18:41:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+setcookie("firrst name" , "osama");
+setcookie("last name" , "Rababah");
+setcookie("array[fname]" , "Bashar");
+setcookie("array[lname]" , "Shboul");
+
+?>
+
+
+<html>
+<head> <title>  setting cocckis </title> </head>
+</html>
