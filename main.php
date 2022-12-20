0d5ab22e4f6eb5afc4f998ba89d12cd408a45ff8
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2022-12-20 23:31:02 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+echo "<h1> Welocome $_SESSION[u] </h1>";
+
+/*
+echo "cookies in this page <br> <br>";
+echo $_COOKIE['username'] . "<br/>" . $_COOKIE['password'];
+
+echo "<br>session in this page <br> <br>";
+echo $_SESSION['u'] . "<br/>".  $_SESSION['p'];
+*/
+
+?>
+
